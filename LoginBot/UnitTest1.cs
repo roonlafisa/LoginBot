57e9e63f9095444c8f051e87d89e3135eb41af25
Asif Al Noor <roonlafisa@gmail.com>
2021-07-05 22:06:18 -0700

    Add project files.

---
+++ LoginBot/UnitTest1.cs
@@ -0,0 +1,53 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace LoginBot
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            //Browser Driver
+            IWebDriver webDriver = new ChromeDriver();
+            webDriver.Manage().Window.Maximize();
+
+            //Navigate
+            webDriver.Navigate().GoToUrl("http://eaapp.somee.com/");
+
+            //Identify Login
+            IWebElement lnkLogin = webDriver.FindElement(By.LinkText("Login"));
+
+            //Click
+            lnkLogin.Click();
+
+            //Username
+            var txtUserName = webDriver.FindElement(By.Name("UserName"));
+
+            //Verify or Assert that Username is displayed
+            Assert.That(txtUserName.Displayed, Is.True);
+
+            //Write Username
+            txtUserName.SendKeys("admin");
+
+            //Write Password
+            webDriver.FindElement(By.Name("Password")).SendKeys("password");
+
+            //Identify and click Login
+            webDriver.FindElement(By.XPath("//input[@value='Log in']")).Submit();
+
+            //Username
+            var txtEmployeeDetails = webDriver.FindElement(By.Name("Employee Details"));
+
+            //Verify or Assert that Username is displayed
+            Assert.That(txtEmployeeDetails.Displayed, Is.True);
+        }
+    }
+}
